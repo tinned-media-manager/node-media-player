63f6a18b0f3bdb38567f2f887fcbfb4ef517f491
Nick <dambergn@gmail.com>
2019-01-30 00:33:42 -0800

    system is now able to send audio via api call

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Self hosted streaming music service.
+
+[] Basic music player that can play a list of MP3's
+[] Set up API to host files to music player
--- index.js
+++ index.js
@@ -15,9 +15,26 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.sendFile('public/index.html', { root: './public' });
-
 })
+
+app.route('/api/audio/:audio').get((req, res) => {
+  console.log('audio hit:', req.params.audio);
+  // console.log('requested file: ', req.params.image)
+  // console.log('dirname: ', `${reqPath}/${req.params.image}`);
+
+  res.set('Content-Type', 'audio/mpeg');
+  res.sendFile(`music/${req.params.audio}`, { root: './public' });
+});
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT, 'use CTRL+C to close.')
 })
+
+let music = fs.readdirSync('public/music');
+// console.log(music);
+
+// let musicList = JSON.stringify(music)
+// console.log(musicList);
+
+// works but is deprecaited and causes app to crash
+// fs.writeFile('./data.json', JSON.stringify(music, null, 2) , 'utf-8');
